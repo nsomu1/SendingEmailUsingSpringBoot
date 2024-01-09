b6d6a453f5397f2568ac1d3cfe365f69a31b286d
nsomu1 <nsomu1@binghamton.edu>
2024-01-09 10:35:39 -0500

    first commit

---
+++ src/main/java/com/example/SpringEmailDemo/SpringEmailDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringEmailDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmailDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailDemoApplication.class, args);
+	}
+
+}
